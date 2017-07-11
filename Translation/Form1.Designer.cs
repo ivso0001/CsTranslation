c0baaec4bed5357c80e198ae716a637dc28d1991
Bang <ivso0001>
2017-07-11 23:45:27 +0900

    Ctrl + C Translation

---
+++ Translation/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Translation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this._editSourceText = new System.Windows.Forms.RichTextBox();
+            this._editTarget = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._comboFrom = new System.Windows.Forms.ComboBox();
+            this._comboTo = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(234, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "State";
+            // 
+            // _editSourceText
+            // 
+            this._editSourceText.Location = new System.Drawing.Point(12, 71);
+            this._editSourceText.Name = "_editSourceText";
+            this._editSourceText.Size = new System.Drawing.Size(260, 80);
+            this._editSourceText.TabIndex = 2;
+            this._editSourceText.Text = "";
+            // 
+            // _editTarget
+            // 
+            this._editTarget.Location = new System.Drawing.Point(12, 169);
+            this._editTarget.Name = "_editTarget";
+            this._editTarget.Size = new System.Drawing.Size(260, 80);
+            this._editTarget.TabIndex = 3;
+            this._editTarget.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 11);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ComboFrom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(121, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 11);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ComboTo";
+            // 
+            // _comboFrom
+            // 
+            this._comboFrom.FormattingEnabled = true;
+            this._comboFrom.Location = new System.Drawing.Point(14, 27);
+            this._comboFrom.Name = "_comboFrom";
+            this._comboFrom.Size = new System.Drawing.Size(73, 20);
+            this._comboFrom.TabIndex = 6;
+            // 
+            // _comboTo
+            // 
+            this._comboTo.FormattingEnabled = true;
+            this._comboTo.Location = new System.Drawing.Point(123, 27);
+            this._comboTo.Name = "_comboTo";
+            this._comboTo.Size = new System.Drawing.Size(73, 20);
+            this._comboTo.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gulim", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(10, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 11);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Translation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gulim", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(12, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 11);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Text:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this._comboTo);
+            this.Controls.Add(this._comboFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._editTarget);
+            this.Controls.Add(this._editSourceText);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GoogleTranslatorFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox _editSourceText;
+        private System.Windows.Forms.RichTextBox _editTarget;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox _comboFrom;
+        private System.Windows.Forms.ComboBox _comboTo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Translation/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Linq;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+namespace Translation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //맨 앞으로
+            this.TopMost = true;
+            //clipboard 리스너 등록
+            AddClipboardFormatListener(this.Handle);
+        }
+
+        //_comboFrom, _comboTo초기화 함수
+        private void GoogleTranslatorFrm_Load
+              (object sender,
+               EventArgs e)
+        {
+            //번역 가능 언어 리스트 넣어주고 초기값을 Korean과 English로줌
+            this._comboFrom.Items.AddRange(Translator.Languages.ToArray());
+            this._comboTo.Items.AddRange(Translator.Languages.ToArray());
+            this._comboFrom.SelectedItem = "English"; 
+            this._comboTo.SelectedItem = "Korean";
+        }
+        
+        private void GoogleTranslator(String text)
+        {
+            // translator 초기화
+            Translator t = new Translator();
+
+            this._editTarget.Text = string.Empty;
+            this._editTarget.Update();
+            //this._translationSpeakUrl = null;/
+
+            // 텍스트 번역 해줌
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                this.label3.Text = "Translating...";
+                this.label3.Update();
+                // t.Translate(복사한 값, from 국가, to 국가)
+                this._editTarget.Text = t.Translate(text.Trim(), (string)this._comboFrom.SelectedItem, (string)this._comboTo.SelectedItem);
+                if (t.Error == null)
+                {
+                    this._editTarget.Update();
+                    //this._translationSpeakUrl = t.TranslationSpeechUrl;/
+                }
+                else
+                {
+                    MessageBox.Show(t.Error.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                this.label3.Text = string.Format("Translated in {0} mSec", (int)t.TranslationTime.TotalMilliseconds);
+                this.Cursor = Cursors.Default;
+            }
+        }
+        
+        /// <summary>
+        /// Places the given window in the system-maintained clipboard format listener list.
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Removes the given window from the system-maintained clipboard format listener list.
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Sent when the contents of the clipboard have changed.
+        /// </summary>
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+        
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                _editSourceText.Text += Clipboard.GetText() + "\n";
+            
+                GoogleTranslator(Clipboard.GetText());
+
+                /*
+             
+                //IDataObject iData = Clipboard.GetDataObject();// Clipboard's data.
+                //Depending on the clipboard's current data format we can process the data differently. 
+                //Feel free to add more checks if you want to process more formats. 
+     
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    string text = (string)iData.GetData(DataFormats.Text);
+                    // do something with it
+                }
+                else if (iData.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap image = (Bitmap)iData.GetData(DataFormats.Bitmap);
+                    // do something with it
+                }
+
+                */
+            }
+        }
+
+        //form이 종료될때 Clipboard 리스너 등록 해제
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RemoveClipboardFormatListener(this.Handle);// Remove our window from the clipboard's format listener list.
+        }
+
+        //private string _translationSpeakUrl;/
+    }
+}
